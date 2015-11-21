1ba1f3faf0ca46cf9cbcbecb20063f1b43b71828
Alex Pearson <pear7@pdx.edu>
2015-11-21 10:35:22 -0800

    Added List Activity.

---
+++ BusinessCardContact/app/src/main/java/edu/pdx/ece558_fall15/alex_elizabeth/businesscardcontact/ContactDetailActivity.java
@@ -0,0 +1,7 @@
+package edu.pdx.ece558_fall15.alex_elizabeth.businesscardcontact;
+
+/**
+ * Created by Alex on 11/21/2015.
+ */
+public class ContactDetailActivity {
+}
--- BusinessCardContact/app/src/main/java/edu/pdx/ece558_fall15/alex_elizabeth/businesscardcontact/ContactListActivity.java
+++ BusinessCardContact/app/src/main/java/edu/pdx/ece558_fall15/alex_elizabeth/businesscardcontact/ContactListActivity.java
@@ -1,6 +1,12 @@
 package edu.pdx.ece558_fall15.alex_elizabeth.businesscardcontact;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 public class ContactListActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
