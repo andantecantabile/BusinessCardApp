dbfd392ea492653b55e02401251c5d7cbd72cdcf
Alex Pearson <pear7@pdx.edu>
2015-11-26 20:00:21 -0800

    Added comments and JavaDoc throughout the first half of the ContactContentResolverHelper class.

--- BusinessCardContact/app/src/main/java/edu/pdx/ece558_fall15/alex_elizabeth/businesscardcontact/ContactContentResolverHelper.java
+++ BusinessCardContact/app/src/main/java/edu/pdx/ece558_fall15/alex_elizabeth/businesscardcontact/ContactContentResolverHelper.java
@@ -25,16 +25,34 @@ import static android.provider.ContactsContract.CommonDataKinds.Website;
 import static android.provider.ContactsContract.Data;
 import static android.provider.ContactsContract.RawContacts;
 
+/**
+ * Class to facilitate storage of ContactEntry objects into the SQLite backed
+ * Android Contact Storage.
+ */
 public class ContactContentResolverHelper {
     private static final String TAG = "ContactCntResolvHelper";
-    private static final String ACT_TYPE = "edu.pdx.ece558_fall15.alex_elizabeth.businesscardcontact";
 
+    //This is the account type that we will store the contact information under
+    private static final String ACT_TYPE =
+            "edu.pdx.ece558_fall15.alex_elizabeth.businesscardcontact";
+
+    //Context under which this is called
     private Context mContext;
 
+    /**
+     * Constructor to create an instance of the ContactContentResolverHelper
+     * that is used to add, modify, deleted, and get items from the Android
+     * Contact Storage
+     * @param context the context which this is called from
+     */
     public ContactContentResolverHelper(Context context) {
         mContext = context;
     }
 
+    /**
+     * Adds a new contact to the Android Contact Storage
+     * @param contactEntry the ContactEntry to add
+     */
     public void addNewContact(ContactEntry contactEntry) {
         Log.d(TAG, "addNewContact");
         long rawContactId = addRawContact(contactEntry.getId());
@@ -48,6 +66,11 @@ public class ContactContentResolverHelper {
         //addPhotoData(rawContactId, contactEntry);
     }
 
+    /**
+     * Updates a currently existing contact in the Android
+     * Contact Storage
+     * @param contactEntry the updated ContactEntry
+     */
     public void updateContact(ContactEntry contactEntry) {
         Log.d(TAG, "updateContact");
         long rawContactId = getRawContact(contactEntry.getId());
@@ -61,9 +84,17 @@ public class ContactContentResolverHelper {
         //updatePhotoData(rawContactId, contactEntry);
     }
 
+    /**
+     * Returns a list of ContactEntry objects from the Android
+     * Contact Storage
+     * @return a list of ContactEntry objects
+     */
     public List<ContactEntry> getAllContacts() {
         Log.d(TAG, "getAllContacts");
         List<ContactEntry> contactEntries = new ArrayList<>();
+
+        //Select all RawContacts _ID and ACCOUNT_NAME columns where the account
+        //type matches our account type
         ContactEntryCursorWrapper cursor = new ContactEntryCursorWrapper(
                 mContext.getContentResolver().query(
                         RawContacts.CONTENT_URI,
@@ -77,113 +108,224 @@ public class ContactContentResolverHelper {
         //Navigate through the cursor
         cursor.moveToFirst();
         while(!cursor.isAfterLast()) {
-            Log.d(TAG, "NEXT ITEM");
+            //Extract the RawContact _ID and the UUID
             long rawContactId = Long.valueOf(cursor.getString(0));
             UUID uuid = UUID.fromString(cursor.getString(1));
+
+            //Get the contact data using the RawContact _ID and the UUID
+            //and add to the list to return
             contactEntries.add(getContact(rawContactId, uuid));
-            Log.d(TAG, "Name: " + contactEntries.get(contactEntries.size() - 1).getName());
             cursor.moveToNext();
         }
+        //Close the cursor to not leak resources
         cursor.close();
 
+        //Return the list of ContactEntry objects
         return contactEntries;
     }
 
+    /**
+     * Deletes a contact from the Android Contact Storage
+     * @param contactEntry the ContactEntry to delete
+     */
     public void deleteContact(ContactEntry contactEntry) {
         Log.d(TAG, "deleteContact");
+
+        //Build a Uri for the RawContacts table and add a query parameter that
+        //indicates we are a "SyncAdapter" meaning that the contact is entirely removed
+        //and not just flagged for deletion.
         Uri contentSyncAdapter = RawContacts.CONTENT_URI.buildUpon()
                 .appendQueryParameter(ContactsContract.CALLER_IS_SYNCADAPTER, "true")
                 .build();
-        //TODO: Add to where clause check against ACT_TYPE
+
+        //Delete the contact only if the account name matches the specified Id and
+        //the account type matches our account type
         int numRowsDeleted = mContext.getContentResolver()
                 .delete(contentSyncAdapter,
-                        RawContacts.ACCOUNT_NAME + "='" + contactEntry.getId().toString() + "'",
+                        RawContacts.ACCOUNT_NAME + "='" + contactEntry.getId().toString() +
+                                "' AND " + RawContacts.ACCOUNT_TYPE + "='" + ACT_TYPE + "'",
                         null);
+
+        //If for some reason something goes wrong and we delete multiple contacts, log an
+        //error message
         if(numRowsDeleted > 1) {
-            Log.e(TAG, "Multiple Contacts Updated in Error");
+            Log.e(TAG, "Multiple Contacts Deleted in Error");
         }
     }
 
+    /**
+     * Add a RawContact of our type with the specified UUID
+     * @param uuid the UUID for the ContactEntry
+     * @return the rawContactId for the just inserted RawContact
+     */
     private long addRawContact(UUID uuid) {
         Log.d(TAG, "addRawContact");
+
+        //Create a ContentValues with the specified account type and account name (the UUID)
         ContentValues person = new ContentValues();
         person.put(RawContacts.ACCOUNT_TYPE, ACT_TYPE);
         person.put(RawContacts.ACCOUNT_NAME, uuid.toString());
+
+        //Insert into the RawContacts table
         Uri rawContactUri = mContext.getContentResolver()
                 .insert(RawContacts.CONTENT_URI, person);
+
+        //Return the rawContactId for the just inserted RawContact entry
         return ContentUris.parseId(rawContactUri);
     }
 
+    /**
+     * Add a Data table entry for the name data
+     * @param rawContactId the RawContact _ID to use as a foreign key
+     * @param contactEntry the ContactEntry to get the data from
+     */
     private void addNameData(long rawContactId, ContactEntry contactEntry) {
         Log.d(TAG, "addNameData");
+
+        //Create a ContentValues with the RawContact _ID and a type of StructuredName
         ContentValues values = new ContentValues();
         values.put(Data.RAW_CONTACT_ID, rawContactId);
         values.put(Data.MIMETYPE, StructuredName.CONTENT_ITEM_TYPE);
+
+        //Add all of the name values from the ContactEntry
         addNameValues(contactEntry, values);
+
+        //Insert the Data table entry for the name data
         Uri dataUri = mContext.getContentResolver()
                 .insert(Data.CONTENT_URI, values);
     }
 
+    /**
+     * Add a Email table entry for the email data
+     * @param rawContactId the RawContact _ID to use as a foreign key
+     * @param contactEntry the ContactEntry to get the data from
+     */
     private void addEmailData(long rawContactId, ContactEntry contactEntry) {
         Log.d(TAG, "addEmailData");
+
+        //Create a ContentValues with the RawContact _ID and a type of Email
         ContentValues values = new ContentValues();
         values.put(Data.RAW_CONTACT_ID, rawContactId);
         values.put(Data.MIMETYPE, Email.CONTENT_ITEM_TYPE);
+
+        //Add all of the email values for the ContactEntry
         addEmailValues(contactEntry, values);
+
+        //Insert the Data table entry for the email data
         Uri dataUri = mContext.getContentResolver()
                 .insert(Data.CONTENT_URI, values);
     }
 
+    /**
+     * Add a Phone table entry for the phone data
+     * @param rawContactId the RawContact _ID to use as a foreign key
+     * @param contactEntry the ContactEntry to get the data from
+     */
     private void addPhoneData(long rawContactId, ContactEntry contactEntry) {
         Log.d(TAG, "addPhoneData");
+
+        //Create a ContentValues with the RawContact _ID and a type of Phone
         ContentValues values = new ContentValues();
         values.put(Data.RAW_CONTACT_ID, rawContactId);
         values.put(Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE);
+
+        //Add all of the phone values for the ContactEntry
         addPhoneValues(contactEntry, values);
+
+        //Insert the Data table entry for the phone data
         Uri dataUri = mContext.getContentResolver()
                 .insert(Data.CONTENT_URI, values);
     }
 
+    /**
+     * Add a Phone table entry for the fax data
+     * @param rawContactId the RawContact _ID to use as a foreign key
+     * @param contactEntry the ContactEntry to get the data from
+     */
     private void addFaxData(long rawContactId, ContactEntry contactEntry) {
         Log.d(TAG, "addFaxData");
+
+        //Create a ContentValues with the RawContact _ID and a type of Phone
         ContentValues values = new ContentValues();
         values.put(Data.RAW_CONTACT_ID, rawContactId);
         values.put(Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE);
+
+        //Add all of the fax values for the ContactEntry
         addFaxValues(contactEntry, values);
+
+        //Insert the Data table entry for the fax data
         Uri dataUri = mContext.getContentResolver()
                 .insert(Data.CONTENT_URI, values);
     }
 
+    /**
+     * Add a Organization table entry for the company data
+     * @param rawContactId the RawContact _ID to use as a foreign key
+     * @param contactEntry the ContactEntry to get the data from
+     */
     private void addCompanyData(long rawContactId, ContactEntry contactEntry) {
         Log.d(TAG, "addCompanyData");
+
+        //Create a ContentValues with the RawContact _ID and a type of Organization
         ContentValues values = new ContentValues();
         values.put(Data.RAW_CONTACT_ID, rawContactId);
         values.put(Data.MIMETYPE, Organization.CONTENT_ITEM_TYPE);
+
+        //Add all of the company values for the ContactEntry
         addCompanyValues(contactEntry, values);
+
+        //Insert the Data table entry for the company data
         Uri dataUri = mContext.getContentResolver()
                 .insert(Data.CONTENT_URI, values);
     }
 
+    /**
+     * Add a Website table entry for the website data
+     * @param rawContactId the RawContact _ID to use as a foreign key
+     * @param contactEntry the ContactEntry to get the data from
+     */
     private void addWebsiteData(long rawContactId, ContactEntry contactEntry) {
         Log.d(TAG, "addWebsiteData");
+
+        //Create a ContentValues with the RawContact _ID and a type of Website
         ContentValues values = new ContentValues();
         values.put(Data.RAW_CONTACT_ID, rawContactId);
         values.put(Data.MIMETYPE, Website.CONTENT_ITEM_TYPE);
+
+        //Add all of the website values for the ContactEntry
         addWebsiteValues(contactEntry, values);
+
+        //Insert the Data table entry for the website data
         Uri dataUri = mContext.getContentResolver()
                 .insert(Data.CONTENT_URI, values);
     }
 
+    /**
+     * Add a Note table entry for the note data
+     * @param rawContactId the RawContact _ID to use as a foreign key
+     * @param contactEntry the ContactEntry to get the data from
+     */
     private void addNoteData(long rawContactId, ContactEntry contactEntry) {
         Log.d(TAG, "addNoteData");
+
+        //Create a ContentValues with the RawContact _ID and a type of Note
         ContentValues values = new ContentValues();
         values.put(Data.RAW_CONTACT_ID, rawContactId);
         values.put(Data.MIMETYPE, Note.CONTENT_ITEM_TYPE);
+
+        //Add all of the note values for the ContactEntry
         addNoteValues(contactEntry, values);
+
+        //Insert the Data table entry for the note data
         Uri dataUri = mContext.getContentResolver()
                 .insert(Data.CONTENT_URI, values);
     }
 
+    /**
+     * Add a Photo table entry for the photo data
+     * @param rawContactId the RawContact _ID to use as a foreign key
+     * @param contactEntry the ContactEntry to get the data from
+     */
     private void addPhotoData(long rawContactId, ContactEntry contactEntry) {
         Log.d(TAG, "addPhotoData");
         ContentValues values = new ContentValues();
