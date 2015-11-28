e4b60c6960c8d3b2f40e7dd147e608ad1fb1b605
Alex Pearson <pear7@pdx.edu>
2015-11-28 10:21:11 -0800

    Base extended AsyncTask class that handles the Dialog for us.   To be extended to show progress to the user for a variety of tasks.

---
+++ BusinessCardContact/app/src/main/java/edu/pdx/ece558_fall15/alex_elizabeth/businesscardcontact/DialogAsyncTask.java
@@ -0,0 +1,40 @@
+package edu.pdx.ece558_fall15.alex_elizabeth.businesscardcontact;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+
+public abstract class DialogAsyncTask extends AsyncTask<String, String, Boolean> {
+    private String mInitialStatus;
+    private ProgressDialog mProgressDialog;
+    private ContactEntry mContactEntry = new ContactEntry();
+    private Callbacks mCallbacks;
+
+    public interface Callbacks {
+        void onAsyncTaskFinished(ContactEntry contactEntry, boolean success);
+    }
+
+    public DialogAsyncTask(String initialStatus, Context context, Callbacks callbacks) {
+        mInitialStatus = initialStatus;
+        mCallbacks = callbacks;
+        mProgressDialog = new ProgressDialog(context);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        mProgressDialog.setMessage(mInitialStatus);
+        mProgressDialog.setCancelable(false);
+        mProgressDialog.setCanceledOnTouchOutside(false);
+        mProgressDialog.show();
+    }
+
+    @Override
+    protected void onPostExecute(Boolean result) {
+        if(mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+
+        mCallbacks.onAsyncTaskFinished(mContactEntry, result);
+    }
+}
