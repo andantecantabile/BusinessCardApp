1c49fd0c0732928fa376cf75042420da5e81a02d
Alex Pearson <pear7@pdx.edu>
2015-11-29 15:54:49 -0800

    Updated imports.

--- BusinessCardContact/app/src/main/java/edu/pdx/ece558_fall15/alex_elizabeth/businesscardcontact/ContactListActivity.java
+++ BusinessCardContact/app/src/main/java/edu/pdx/ece558_fall15/alex_elizabeth/businesscardcontact/ContactListActivity.java
@@ -1,28 +1,22 @@
 package edu.pdx.ece558_fall15.alex_elizabeth.businesscardcontact;
 
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.UUID;
 
 public class ContactListActivity extends AppCompatActivity
