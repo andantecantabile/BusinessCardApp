0265da7b4b68430484bbefbfc987236a41d209e0
Elizabeth R <ereed@pdx.edu>
2015-11-28 21:31:02 -0800

    In landscape mode, both the contact list and the contact detail views are correctly updated on deletion of a contact.

--- BusinessCardContact/app/src/main/java/edu/pdx/ece558_fall15/alex_elizabeth/businesscardcontact/ContactListActivity.java
+++ BusinessCardContact/app/src/main/java/edu/pdx/ece558_fall15/alex_elizabeth/businesscardcontact/ContactListActivity.java
@@ -62,13 +62,7 @@ public class ContactListActivity extends AppCompatActivity
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         Log.d(TAG, "onSaveInstanceState");
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.detail_fragment_container);
-        if(fragment != null) {
-            getSupportFragmentManager().beginTransaction()
-                    .remove(fragment)
-                    .commit();
-        }
+        removeDetailFragmentUI();   // remove the detail fragment
         super.onSaveInstanceState(outState);
     }
 
@@ -161,64 +155,9 @@ public class ContactListActivity extends AppCompatActivity
     public void onContactEntryDelete(ContactEntry ce) {
         Log.d(TAG, "onContactEntryDelete");
 
-        FragmentManager fm = getSupportFragmentManager();
-        //Check if the id for placing the ContactListFragment in exists
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-        if ((fragment != null) && (fragment instanceof ContactListFragment)) {
-            ContactListFragment clFragment = (ContactListFragment) fragment;
-            clFragment.refreshUI();
-        }
-        /*
-        mCurrContactEntry = ce;
-
-        // TODO: before checking for confirmation from user that the entry should be deleted... may need to check first that the given contact entry is not null... (but this case shouldn't actually happen...)
-
-        // get confirmation from user in a dialog that they want to go back without saving changes
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        builder.setTitle("Delete this contact?");
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                // Yes, the user wants to delete the contact...
-
-                // delete the currently selected contact entry
-                ContactStore.get(getApplicationContext()).deleteContactEntry(mCurrContactEntry);
-                //ContactStore.get(this).deleteContactEntry(ce);
-
-                // after deletion, need to refresh the list view.
-                FragmentManager fm = getSupportFragmentManager();
-                // find the list fragment and update
-                Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-                if(fragment != null) {
-                    if (fragment instanceof ContactListFragment) {
-                        ContactListFragment listFragment = (ContactListFragment) fragment;
-                        listFragment.updateUI();
-                    }
-                }
-
-                // check for the existence of the detailfragmentcontainer,
-                // if it is present, it needs to be cleared.
-                Fragment detailFragment = fm.findFragmentById(R.id.detail_fragment_container);
-                if(detailFragment != null) {
-                    getSupportFragmentManager().beginTransaction()
-                            .remove(detailFragment)
-                            .commit();
-                }
-            }
-        });
-
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                // Cancel the dialog here, if the user decides not to delete the currently active contact entry.
-                dialog.cancel();
-            }
-        });
-
-        AlertDialog alert = builder.create();
-        alert.show();
-        */
+        // Need to update the views of the fragments.
+        updateListFragmentUI();     // update list view
+        removeDetailFragmentUI();   // clear detail view
     }
 
     @Override
@@ -245,36 +184,63 @@ public class ContactListActivity extends AppCompatActivity
         Log.d(TAG, "onResume");
 
         // Need to update the views of the fragments.
+        updateListFragmentUI();
+        updateDetailFragmentUI();
+    }
+
+    /**
+     * This method is used to update the list fragment UI display.
+     */
+    public void updateListFragmentUI() {
+        // Need to update the views of the fragments.
 
         FragmentManager fm = getSupportFragmentManager();
         // find the list fragment
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-        if(fragment != null) {
+        if (fragment != null) {
             if (fragment instanceof ContactListFragment) {
                 ContactListFragment listFragment = (ContactListFragment) fragment;
-                listFragment.updateUI();
+                listFragment.refreshUI();   // refresh the UI for the contact list
             }
         }
+    }
 
+    /**
+     * This method is used to update the detail fragment UI
+     */
+    public void updateDetailFragmentUI() {
         // check for the existence of the detailfragmentcontainer,
         // if it is present, the view needs to be updated.
+        FragmentManager fm = getSupportFragmentManager();
         Fragment detailFragment = fm.findFragmentById(R.id.detail_fragment_container);
         if(detailFragment != null) {
             if (detailFragment instanceof ContactDetailFragment) {
                 ContactDetailFragment cdFragment = (ContactDetailFragment) detailFragment;
                 cdFragment.updateUI();
             }
-            /*
-            getSupportFragmentManager().beginTransaction()
-                    .detach(detailFragment)
-                    .commit();
-            getSupportFragmentManager().beginTransaction()
-                    .attach(detailFragment)
-                    .commit();
-            */
         }
     }
 
+    /**
+     * Method to remove the detail fragment; if it is present. This has the result of clearing the detail view.
+     */
+    public void removeDetailFragmentUI() {
+        // Find the detail fragment; if it is present, then remove it from the fragment manager.
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.detail_fragment_container);
+        if(fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .remove(fragment)
+                    .commit();
+        }
+    }
+
+    /**
+     * This method is the callback when async tasks have finished.
+     * @param contactEntry  The returned ContactEntry from the async task (may or may not be applicable, depending on the async task)
+     * @param success   Should return true if the async task was successful.
+     * @param taskId    The id of the async task that has completed.
+     */
     @Override
     public void onAsyncTaskFinished(ContactEntry contactEntry, boolean success, int taskId) {
         if(success) {
